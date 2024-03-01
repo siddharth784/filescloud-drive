ba4270d669deb3371eb16170e6726d0939ebaebe
Siddharth Chauhan <mesiddharth.001@gmail.com>
2024-03-02 01:09:02 +0530

    update

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import "./css/header.css"
 import logo from './components/cloudlogo.png';
-// import SearchIcon from '@mui/icons-material/Search';
 // import SearchIcon from 'https://fontawesome.com/icons/magnifying-glass?f=classic&s=solid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { faSliders } from '@fortawesome/free-solid-svg-icons';
-// import { faGear } from '@fortawesome/free-solid-svg-icons';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-// import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import { faGrip } from '@fortawesome/free-solid-svg-icons';
-// import { AppsIcon } from '@mui/icons-material/Apps';
 import AppsIcon from '@mui/icons-material/Apps';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'></link>
